a0c6d6ab6f8e124871192cac5863352df748bea0
ablades <audaris@gmail.com>
2020-07-14 16:00:33 -0500

    complete bfs function

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
+//NodeTypes
+const (
+	SOURCE int = 0
+	DIE    int = 1
+	LETTER int = 2
+	SINK   int = 3
+)
+
+//Node component of graph
 type Node struct {
 	ID       int
 	Letters  []bool
 	Adj      []*Edge //Adjacency List
-	NodeType string  // type of node source, sink, die, letter
+	Type     int     // type of node source, sink, die, letter
 	Visited  bool
 	BackEdge *Edge
 }
 
 //NewNode creates a new node
-func NewNode(id int, nodeType string) Node {
+func NewNode(id int, nodeType int) Node {
 	n := Node{
 		ID:      id,
 		Letters: make([]bool, 26),
+		Type:    nodeType,
 	}
 	return n
 }
@@ -23,7 +33,7 @@ type Edge struct {
 	From     *Node
 	To       *Node
 	Reverse  *Edge //Edge in the opposite direction
-	Origin   int   // Flow Field
+	Original int   // Flow Field
 	Residual int   // Flow Field
 	//TODO: Init reverse edge inplace
 }
@@ -37,7 +47,7 @@ func NewEdge(from *Node, to *Node) {
 		From:     from,
 		To:       to,
 		Reverse:  &reverseEdge,
-		Origin:   1,
+		Original: 1,
 		Residual: 0,
 	}
 
@@ -45,7 +55,61 @@ func NewEdge(from *Node, to *Node) {
 		From:     to,
 		To:       from,
 		Reverse:  &edge,
-		Origin:   0,
+		Original: 0,
 		Residual: 1,
 	}
+}
+
+//Graph contains nodes and all helper functions
+type Graph struct {
+	Nodes    []*Node
+	NodeIDS  map[int]int
+	minNodes int
+}
+
+//bfs breath first search on graph
+func (g Graph) bfs() bool {
+	queue := make([]int, 10)
+	queue = append(queue, SOURCE)
+
+	//Continue until queue is empty
+	for len(queue) > 0 {
+		//dequeue item
+		nodeID := queue[0]
+		queue = queue[1:]
+
+		//mark node as visited
+		g.Nodes[nodeID].Visited = true
+		var nextNode *Node
+		var edge *Edge
+
+		//Iterate over adj list
+		for _, node := range g.Nodes[nodeID].Adj {
+			nextNode = node.To
+			edge = node
+
+			//Add unvisited nodes flowing in the right direction to queue
+			if nextNode.Visited == false && edge.Original == 1 {
+				//Set the backedge for the node
+				nextNode.BackEdge = edge.Reverse
+				//Add to queue
+				queue = append(queue, nextNode.ID)
+
+				//Path has been found
+				if nextNode.Type == SINK {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func (g Graph) canSpell() bool {
+
+}
+
+//resetNodes adj list resid/origi
+func (g Graph) resetNodes() {
+
 }
